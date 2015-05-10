d3ec159a5b36dd6f8a68683764b419a671c5e2a2
youssef <youssef@YLAPTOP>
2015-05-10 17:13:08 +0100

    Cropped gmaps on view change fixed

--- lib/decorator/gmap_decorator.dart
+++ lib/decorator/gmap_decorator.dart
@@ -2,7 +2,6 @@ part of decorator;
 
 @Decorator(selector: '[gmap]')
 class GMapDecorator implements AttachAware, DetachAware {
-
   final Element element;
   GMap _gmap;
   num _zoom;
@@ -18,54 +17,51 @@ class GMapDecorator implements AttachAware, DetachAware {
   @NgCallback('on_created')
   Function onCreated;
 
-  GMapDecorator(this.element) {
-    _gmap = new GMap(element);
-  }
+  GMapDecorator(this.element);
 
   @NgTwoWay('zoom')
   set zoom(num zoom) {
     _zoom = zoom;
-    _gmap.zoom = zoom;
+    if (_gmap != null) _gmap.zoom = zoom;
   }
   get zoom => _zoom;
 
   @NgTwoWay('center')
   set center(LatLng center) {
     _center = center;
-    _gmap.center = center;
+    if (_gmap != null) _gmap.center = center;
   }
   get center => _center;
 
   @NgOneWay('map-type-id')
   set mapTypeId(MapTypeId mapTypeId) {
     _mapTypeId = mapTypeId;
-    _gmap.mapTypeId = mapTypeId;
+    if (_gmap != null) _gmap.mapTypeId = mapTypeId;
   }
 
   attach() {
-    onCreated({
-      "\$gmap": _gmap
-    });
+    _gmap = new GMap(element);
+    zoom = _zoom;
+    center = _center;
+    mapTypeId = _mapTypeId;
+
+    onCreated({"\$gmap": _gmap});
 
     _gmap.onZoomChanged.listen((e) {
       _zoom = _gmap.zoom;
     });
-    
+
     _gmap.onBoundsChanged.listen((e) {
       bounds = _gmap.bounds;
       center = _gmap.center;
     });
 
     _gmap.onClick.listen((MouseEvent e) {
-      onClick({
-        "\$mouseEvent": e
-      });
+      onClick({"\$mouseEvent": e});
     });
-    _gmap.onRightclick.listen((MouseEvent e) => onRightclick({
-      "\$mouseEvent": e
-    }));
+    _gmap.onRightclick
+        .listen((MouseEvent e) => onRightclick({"\$mouseEvent": e}));
   }
 
   detach() {}
-
 }
